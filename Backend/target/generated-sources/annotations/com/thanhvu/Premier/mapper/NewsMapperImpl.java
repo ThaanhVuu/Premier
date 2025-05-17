343375972ec1745445f433bc702728393d57787f
ThaanhVuu <thanhvu7603@gmail.com>
2025-05-18 01:09:24 +0700

    test deploy

--- Backend/target/generated-sources/annotations/com/thanhvu/Premier/mapper/NewsMapperImpl.java
+++ Backend/target/generated-sources/annotations/com/thanhvu/Premier/mapper/NewsMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-17T19:07:06+0700",
+    date = "2025-05-18T01:04:34+0700",
     comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
 )
 @Component
