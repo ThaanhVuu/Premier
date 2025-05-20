d65d4d8df2f85808eeeddd9add9cf1b19ba20043
ThaanhVuu <thanhvu7603@gmail.com>
2025-05-20 08:49:21 +0700

    ignore target file

---
+++ Backend/target/generated-sources/annotations/com/thanhvu/Premier/mapper/StadiumMapperImpl.java
@@ -0,0 +1,47 @@
+package com.thanhvu.Premier.mapper;
+
+import com.thanhvu.Premier.dto.Request.StadiumRequest;
+import com.thanhvu.Premier.entity.Stadium;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-20T08:47:40+0700",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
+)
+@Component
+public class StadiumMapperImpl implements StadiumMapper {
+
+    @Override
+    public Stadium toStadium(StadiumRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        Stadium.StadiumBuilder stadium = Stadium.builder();
+
+        stadium.name( request.getName() );
+        stadium.city( request.getCity() );
+        stadium.capacity( request.getCapacity() );
+        stadium.address( request.getAddress() );
+        stadium.builtYear( request.getBuiltYear() );
+        stadium.photoUrl( request.getPhotoUrl() );
+
+        return stadium.build();
+    }
+
+    @Override
+    public void updateToStadium(StadiumRequest rq, Stadium stadium) {
+        if ( rq == null ) {
+            return;
+        }
+
+        stadium.setName( rq.getName() );
+        stadium.setCity( rq.getCity() );
+        stadium.setCapacity( rq.getCapacity() );
+        stadium.setAddress( rq.getAddress() );
+        stadium.setBuiltYear( rq.getBuiltYear() );
+        stadium.setPhotoUrl( rq.getPhotoUrl() );
+    }
+}
